9b74e85769beef53b33fdbcf536803b3a8693217
alexish4 <alexish4@cpp.edu>
2022-04-22 07:23:28 -0700

    Saving

--- Laser.gd
+++ Laser.gd
@@ -9,3 +9,8 @@ func _process(delta: float) -> void:
 
 func _on_VisibilityNotifier2D_viewport_exited(viewport: Viewport) -> void:
 	queue_free()
+
+
+func _on_Laser_body_shape_entered(body_id: int, body: Node, body_shape: int, area_shape: int) -> void:
+	if (body.is_in_group("enemy")):
+		print("enemy hit")
--- player.gd
+++ player.gd
@@ -14,7 +14,7 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
-var speed : int = 800
+var speed : int = 200
 var vel : Vector2 = Vector2()
 func _physics_process(delta):
 	vel.x = 0
