91b8b57427bb6bd5bd06e21db1a726663512d506
alexish4 <alexish4@cpp.edu>
2022-04-21 18:55:58 -0700

    Can move now

--- player.gd
+++ player.gd
@@ -4,7 +4,7 @@ extends KinematicBody2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-var time_in_run = 0
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -12,9 +12,15 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	#moving left or right
-	if Input.is_action_pressed("ui__right"):
-			time_in_run += delta
-	elif Input.is_action_pressed("ui_left"):
-			time_in_run -= delta
+#func _process(delta):
+#	pass
+var speed : int = 200
+var vel : Vector2 = Vector2()
+func _physics_process(delta):
+	vel.x = 0
+	
+	if Input.is_action_pressed("ui_left"):
+		vel.x -= speed
+	if Input.is_action_pressed("ui_right"):
+		vel.x += speed
+	move_and_slide(vel, Vector2.UP)
