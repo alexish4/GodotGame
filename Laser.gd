df745ca4b14f26768f58028d57eafa02b760bc84
AamirMS <94090582+AamirMS@users.noreply.github.com>
2022-05-01 14:49:51 -0700

    Add files via upload

---
+++ Laser.gd
@@ -0,0 +1,27 @@
+extends Area2D
+
+var direction := Vector2(0, -1)
+var projectile_speed := 1000
+
+func _process(delta: float) -> void:
+	self.position += direction * projectile_speed * delta
+
+
+func _on_VisibilityNotifier2D_viewport_exited(viewport: Viewport) -> void:
+	queue_free()
+
+
+func _on_Laser_body_shape_entered(body_id: int, body: Node, body_shape: int, area_shape: int) -> void:
+	if (body.is_in_group("enemy1")):
+		print("enemy hit")
+
+
+func _on_Hurtbox_body_shape_entered(body_rid, body, body_shape_index, local_shape_index):
+	if (body.is_in_group("enemy1")):
+		print("enemy hit")
+
+
+func _on_Hurtbox_area_entered(area):
+	if area.is_in_group("enemy1"):
+		area.get_parent().queue_free()
+		print("Enemy fired")
---
+++ player.gd
@@ -0,0 +1,30 @@
+extends KinematicBody2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+var speed : int = 800
+var vel : Vector2 = Vector2()
+func _physics_process(delta):
+	vel.x = 0
+	
+	if Input.is_action_pressed("ui_left"):
+		vel.x -= speed
+	if Input.is_action_pressed("ui_right"):
+		vel.x += speed
+	move_and_slide(vel, Vector2.UP)
+	
+func _unhandled_key_input(event: InputEventKey) -> void:
+	if (event.is_action_pressed("shoot")):
+		$LaserWeapon.shoot()
