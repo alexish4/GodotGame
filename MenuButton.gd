0063f23e4a8fd28f507b3c54ea91cb4ba0495951
alexish4 <alexish4@cpp.edu>
2022-05-18 09:14:48 -0700

    Saving

--- MenuButton.gd
+++ MenuButton.gd
@@ -1,9 +1,10 @@
 extends Control
 
 export(PackedScene) var MainMenu
+var loadMenu = load("res://Menu.tscn")
 
 func _ready():
 	pass
 
 func _on_MainMenu_pressed():
-	get_tree().change_scene_to(MainMenu)
+	get_tree().change_scene_to(loadMenu)
